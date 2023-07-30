d0279f85e50d422eeeeaeeb30dc89bc194afd31a
Yan Chummar <yanpooppada@gmail.com>
2023-07-30 21:41:12 +0530

    initial commit

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,7 +1,7 @@
-import './globals.css'
-import { Inter } from 'next/font/google'
-
-const inter = Inter({ subsets: ['latin'] })
+import '../styles/globals.scss'
+import '../styles/index.scss'
+import '../styles/dashboard.scss'
+import '../styles/components/BackgroundGradient.scss'
 
 export const metadata = {
   title: 'Create Next App',
@@ -11,7 +11,12 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <head>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+        <link href="https://fonts.googleapis.com/css2?family=Lexend:wght@100;200;300;400;500;600;700;900&display=swap" rel="stylesheet" />
+      </head>
+      <body>{children}</body>
     </html>
   )
 }
---
+++ src/components/BackgroundGradient/index.js
@@ -0,0 +1,23 @@
+'use client'
+
+import Head from 'next/head';
+import Script from 'next/script';
+import { useEffect } from 'react';
+
+export default function BackgroundGradient(props) {
+
+  const { isWebView } = props
+
+  useEffect(() => {
+    var gradient = new Gradient()
+    gradient.initGradient('#gradient-canvas');
+  }, [])
+  
+  return (
+    <>
+      <Script src="/gradient.js" strategy="beforeInteractive"></Script>
+      <canvas id="gradient-canvas" data-faded={isWebView}></canvas>
+    </>
+  )
+
+}
